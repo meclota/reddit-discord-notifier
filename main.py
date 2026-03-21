8a74d5d6f1e889ca21a53288dafb7e7cba3296e9
meclota <yusufyld7@gmail.com>
2026-03-21 16:22:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FEEDS_FILE = os.path.join(BASE_DIR, "feeds.json")
 LAST_POSTS_FILE = os.path.join(BASE_DIR, "last_posts.json")
 
-TOKEN = os.environ["TOKEN"]
+TOKEN = os.environ["DISCORD_TOKEN"]
 
 # --- VERİ YÖNETİMİ (KESİN ÇÖZÜM) ---
 def load_data(filename):
